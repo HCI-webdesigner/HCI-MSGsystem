0b9e1813f8b37fec8722b3f1d82fe3343a29c947
guanlisha <1258772059@gmail.com>
2013-10-29 23:23:36 +0800

    readNumber.php
    阅读量

---
+++ blog/readNumber.php
@@ -0,0 +1,18 @@
+﻿<?php
+//阅读量
+	$id =$_GET["id"];
+	$arr =array();
+	include "connectSql.php";
+	$sql ="select * from article where id='$id' ";
+	$rs=mysql_query($sql,$connect);
+	if(!$rs) {
+		$arr['isSuccess'] = 'false';
+	}else {
+		$s="update article set r_number=r_number+1";
+		//echo "ok2";
+		mysql_query($s,$connect);
+	}
+	mysql_close($connect);	
+	require "lib/jsonEncode.php";
+	echo my_json($arr);
+?>
