6150dbc70869478e3c8d6c2f5bc83d4cd730052e
guanlisha <1258772059@gmail.com>
2014-01-13 19:53:02 +0800

    Revert "登录"
    This reverts commit 1042abd173e057b53720244b4e250941be6fffb8.

--- back/index.php
+++ back/index.php
@@ -5,10 +5,6 @@
     <title></title>
 </head>
 <body>
-    <form method="post" action="../Controllers/log.php">
-    	账号:<input type="text" name="user"><br>
-    	密码:<input type="password" name="password"><br>
-    	<input type="submit" value="登录">
-    </form>
+    
 </body>
 </html>
