b79902d9bb1c3c52abe905f2ba8e5c22701e2e41
c860 <windwebmaster@163.com>
2014-01-13 21:32:57 +0800

    update the back

---
+++ back/main.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>后台管理页面</title>
+        <link rel="stylesheet" href="../public/stylesheets/back.css">
+    </head>
+    <body>
+        <div class="board" id="board">
+            <nav class="sidebar">
+                <h1>后台管理</h1>
+                <ul>
+                    <li>item1</li>
+                </ul>
+            </nav>
+            <div class="main">
+                <iframe id="framepage" src="welcome.php" frameborder="0"></iframe>
+            </div>
+        </div>
+        <script type="text/javascript" src="../public/javascripts/back.js"></script>
+    </body>
+</html>
