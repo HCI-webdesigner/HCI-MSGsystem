065f0cce2fd0e35374f02de41d308c54a689a6ec
c860 <windwebmaster@163.com>
2014-01-14 20:47:58 +0800

    update the db

--- hcimsg.sql
+++ hcimsg.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2014 年 01 月 14 日 15:48
+-- 生成日期: 2014 年 01 月 14 日 20:47
 -- 服务器版本: 5.5.34-0ubuntu0.13.04.1
 -- PHP 版本: 5.4.9-4ubuntu2.4
 
@@ -36,6 +36,103 @@ CREATE TABLE IF NOT EXISTS `article` (
   `isImportant` int(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `comment`
+--
+
+CREATE TABLE IF NOT EXISTS `comment` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `article_id` int(10) NOT NULL,
+  `reply_id` int(1) NOT NULL DEFAULT '-1',
+  `user_id` int(10) NOT NULL,
+  `content` text COLLATE utf8_unicode_ci NOT NULL,
+  `createTime` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tag`
+--
+
+CREATE TABLE IF NOT EXISTS `tag` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `isFormal` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- 转存表中的数据 `tag`
+--
+
+INSERT INTO `tag` (`ID`, `name`, `isFormal`) VALUES
+(1, '活动通知', 1),
+(2, 'test', 0);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tag_relate_article`
+--
+
+CREATE TABLE IF NOT EXISTS `tag_relate_article` (
+  `article_id` int(10) NOT NULL,
+  `tag_id` int(10) NOT NULL,
+  PRIMARY KEY (`article_id`,`tag_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user_basic`
+--
+
+CREATE TABLE IF NOT EXISTS `user_basic` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `user` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `isAdmin` int(11) NOT NULL DEFAULT '1',
+  `lastLogTime` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `user` (`user`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=15 ;
+
+--
+-- 转存表中的数据 `user_basic`
+--
+
+INSERT INTO `user_basic` (`ID`, `user`, `password`, `isAdmin`, `lastLogTime`) VALUES
+(13, 'test@qq.com', 'test', 0, NULL),
+(14, 'hello@qq.com', 'test', 0, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user_info`
+--
+
+CREATE TABLE IF NOT EXISTS `user_info` (
+  `user_id` int(10) NOT NULL,
+  `nickname` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `popularity` int(10) DEFAULT '0',
+  `registerTime` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `signature` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `article_count` int(10) DEFAULT '0',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- 转存表中的数据 `user_info`
+--
+
+INSERT INTO `user_info` (`user_id`, `nickname`, `popularity`, `registerTime`, `signature`, `article_count`) VALUES
+(13, 'test', 0, '2014-01-12 19:27:08', NULL, 0),
+(14, 'test', 0, '2014-01-13 15:14:11', NULL, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
