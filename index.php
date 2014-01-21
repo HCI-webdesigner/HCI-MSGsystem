7e0bcb57e1934a04ecec0df34e226421a34e9811
winsycwen <winsycwen@gmail.com>
2014-01-21 20:26:40 +0800

    add the log about modification and tagsShow.php in back folder.

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+include_once('Controllers/index.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    
+</body>
+</html>
