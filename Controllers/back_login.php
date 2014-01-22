0c48b194bdd1169eb61f133bdd29b6d1c4cfb8cd
c860 <windwebmaster@163.com>
2014-01-22 16:01:42 +0800

    update the README and backs

---
+++ Controllers/back_login.php
@@ -0,0 +1,7 @@
+<?php
+/*
+ * back_login.php
+ * 负责后台登录逻辑
+ * Created By C860 at 2014-1-22
+ */
+
--- back/index.php
+++ back/index.php
@@ -3,11 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <title></title>
+    <style>
+        .logform {
+            margin: 50px auto;
+            background: #F0FFFF;
+            color: #1E90FF;
+            width: 400px;
+            line-height: 50px;
+            text-align: center;
+            padding: 20px;
+        }
+    </style>
 </head>
 <body>
-    <form method="post" action="../Controllers/log.php">
-    	账号:<input type="text" name="user"><br>
-    	密码:<input type="password" name="password"><br>
+    <form class="logform" method="post" action="../Controllers/back_login.php">
+    	账号<input type="text" name="user"><br>
+    	密码<input type="password" name="password"><br>
     	<input type="submit" value="登录">
     </form>
 </body>
