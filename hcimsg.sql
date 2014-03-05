14f189c002616c437ca091a30bcd2ad9b9ecf4ee
c860 <windwebmaster@163.com>
2014-03-05 23:08:41 +0800

    update the database backup

--- hcimsg.sql
+++ hcimsg.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.8.1deb1
+-- version 3.4.10.1deb1
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2014 年 01 月 21 日 19:10
--- 服务器版本: 5.5.34-0ubuntu0.13.04.1
--- PHP 版本: 5.4.9-4ubuntu2.4
+-- 生成日期: 2014 年 03 月 05 日 23:08
+-- 服务器版本: 5.5.35
+-- PHP 版本: 5.5.9-1+sury.org~precise+1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -34,6 +34,7 @@ CREATE TABLE IF NOT EXISTS `article` (
   `lastModifyTime` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `user_id` int(10) NOT NULL,
   `isImportant` int(1) NOT NULL DEFAULT '0',
+  `comment` int(10) NOT NULL DEFAULT '0',
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
 
@@ -41,10 +42,10 @@ CREATE TABLE IF NOT EXISTS `article` (
 -- 转存表中的数据 `article`
 --
 
-INSERT INTO `article` (`ID`, `title`, `content`, `createTime`, `lastModifyTime`, `user_id`, `isImportant`) VALUES
-(3, 'testtitle', 'testcontent', '2014-01-20 01:59:30', NULL, 13, 0),
-(4, 'testtitle2', 'testcontent2', '2014-01-20 02:00:10', NULL, 13, 0),
-(5, 'testtitle3', 'testcontent3', '2014-01-20 16:16:28', NULL, 13, 0);
+INSERT INTO `article` (`ID`, `title`, `content`, `createTime`, `lastModifyTime`, `user_id`, `isImportant`, `comment`) VALUES
+(3, 'testtitle', 'testcontent', '2014-01-20 01:59:30', NULL, 13, 0, 0),
+(4, 'testtitle2', 'testcontent2', '2014-01-20 02:00:10', NULL, 13, 0, 0),
+(5, 'testtitle3', 'testcontent3', '2014-01-20 16:16:28', NULL, 13, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -70,19 +71,22 @@ CREATE TABLE IF NOT EXISTS `comment` (
 
 CREATE TABLE IF NOT EXISTS `slider` (
   `ID` int(10) NOT NULL AUTO_INCREMENT,
-  `height` int(10) NOT NULL,
+  `weight` int(10) NOT NULL,
   `link` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `img` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
 
 --
 -- 转存表中的数据 `slider`
 --
 
-INSERT INTO `slider` (`ID`, `height`, `link`, `title`, `img`) VALUES
-(1, 2, 'scauhci.org', 'test', 'none.jpg');
+INSERT INTO `slider` (`ID`, `weight`, `link`, `title`, `img`) VALUES
+(1, 1, 'scauhci.org', 'test1', './public/images/1.jpg'),
+(3, 0, 'scauhci.org', 'test', './public/images/lanlan.png'),
+(4, 2, 'scauhci.org', 'test2', './public/images/2.jpg'),
+(5, 3, 'scauhci.org', 'test3', './public/images/3.jpg');
 
 -- --------------------------------------------------------
 
@@ -94,6 +98,7 @@ CREATE TABLE IF NOT EXISTS `tag` (
   `ID` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `isFormal` int(1) NOT NULL DEFAULT '0',
+  `img` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
 
@@ -101,13 +106,13 @@ CREATE TABLE IF NOT EXISTS `tag` (
 -- 转存表中的数据 `tag`
 --
 
-INSERT INTO `tag` (`ID`, `name`, `isFormal`) VALUES
-(1, '活动通知', 1),
-(2, 'TCP/IP', 0),
-(3, 'Apache', 0),
-(4, 'HTML5', 0),
-(5, 'CSS3', 0),
-(6, 'Javascript', 0);
+INSERT INTO `tag` (`ID`, `name`, `isFormal`, `img`) VALUES
+(1, '活动通知', 1, './public/images/box3.png'),
+(2, 'TCP/IP', 0, NULL),
+(3, 'Apache', 0, NULL),
+(4, 'HTML5', 0, NULL),
+(5, 'CSS3', 0, NULL),
+(6, 'Javascript', 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -130,7 +135,9 @@ INSERT INTO `tag_relate_article` (`article_id`, `tag_id`) VALUES
 (4, 1),
 (4, 3),
 (4, 4),
-(5, 1);
+(5, 1),
+(6, 1),
+(7, 5);
 
 -- --------------------------------------------------------
 
@@ -146,15 +153,16 @@ CREATE TABLE IF NOT EXISTS `user_basic` (
   `lastLogTime` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `user` (`user`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=17 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=18 ;
 
 --
 -- 转存表中的数据 `user_basic`
 --
 
 INSERT INTO `user_basic` (`ID`, `user`, `password`, `isAdmin`, `lastLogTime`) VALUES
-(13, 'test@qq.com', 'testtest', 0, '2014-01-20 17:51:46'),
-(16, 'test2@qq.com', 'test2', 0, NULL);
+(13, 'test@qq.com', 'test', 1, '2014-02-28 13:11:51'),
+(16, 'test2@qq.com', 'test2', 0, '2014-02-03 20:03:22'),
+(17, 'test', 'test', 0, '2014-02-28 12:27:58');
 
 -- --------------------------------------------------------
 
@@ -178,7 +186,8 @@ CREATE TABLE IF NOT EXISTS `user_info` (
 
 INSERT INTO `user_info` (`user_id`, `nickname`, `popularity`, `registerTime`, `signature`, `article_count`) VALUES
 (13, 'test', 5, '2014-01-12 19:27:08', 'test', 1),
-(16, 'test2', 0, '2014-01-18 22:24:11', 'test2', 0);
+(16, 'test2', 0, '2014-01-18 22:24:11', 'test2', 0),
+(17, 'c860', 10, '2014-02-03 20:03:22', NULL, 2);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
